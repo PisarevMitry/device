4eb8cebdf05f275ee4484ec01fa6194639658eaf
Pisarev <d.pisarev.03@mail.ru>
2022-03-13 01:04:07 +0300

    изменил зависимости, чтобы запускать устройство без инициализации БД

--- core/src/main/java/liga/medical/devicecore/CoreApplication.java
+++ core/src/main/java/liga/medical/devicecore/CoreApplication.java
@@ -2,8 +2,10 @@ package liga.medical.devicecore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("liga.medical.device.*")
 public class CoreApplication {
 
     public static void main(String[] args) {
